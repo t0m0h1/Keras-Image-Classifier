aaddbdca3243337fb452b9ffe03849d1f371ed85
t0m0h1 <104913403+t0m0h1@users.noreply.github.com>
2023-07-08 16:36:22 +0100

    changes

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-import cv2 as cv 
-import numpy as np
-import matplotlib.pyplot as plt
-from tensorflow.keras import datasets, layers, models
 
-(training_images, training_labels), (testing_images, testing_labels) = datasets.cifar10.load_data()
-training_images, testing_images = training_images / 255, testing_images / 255 
-
-class_names = ['Plane', 'Car', 'Bird', 'Cat', 'Deer', 'Frog', 'Horse', 'Ship', 'Truck']
+for i in range(16):
+    plt.subplot(4,4,i+1)
+    plt.xticks([])
+    plt.yticks([])
 
 
 
 
+
+# now using google colab 
+# https://colab.research.google.com/drive/1N5KndTvhVyIy_b58ooKbbjKhiJqr-9bi#scrollTo=VgKjkSUEJCaR
